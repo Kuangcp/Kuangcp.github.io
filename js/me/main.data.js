a91f151aedc394c3035a2d85288fa7f21bb46ecc
mythos <kuang.myth@foxmail.com>
2018-01-29 17:24:40 +0800

    add type

--- js/me/main.data.js
+++ js/me/main.data.js
@@ -1,42 +1,45 @@
 list={
     "col":"6",
     "colNum":"2",
-    data:[{
-        "img":"","title":"Github","url":"https://github.com/Kuangcp"
+    button:[{
+        "title":"",
+        "":"",
     },{
-        "title":"码云","url":"https://gitee.com/kcp1104"
+
+    }],
+    fun:[{
+        "title":"知乎","url":"https://www.zhihu.com/"
+    }],
+    platform:[{
+        "title":"微云","url":"https://www.weiyun.com/disk#m=recent"
+    },{
+        "title":"百度网盘","url":"https://pan.baidu.com/disk/home?#list/path=%2F&vmode=list"
     },{
         "title":"七牛云","url":"https://portal.qiniu.com/create"
     },{
         "title":"微信","url":"https://wx.qq.com/"
     },{
         "title":"WebQQ","url":"http://w.qq.com/"
+    }],
+    code:[{
+        "img":"","title":"Github","url":"https://github.com/Kuangcp"
+    },{
+        "title":"SUMMARY.md","url":"https://github.com/Kuangcp/Notes/blob/master/SUMMARY.md"
+    },{
+        "title":"码云","url":"https://gitee.com/kcp1104"
     },{
         "title":"阿里邮箱","url":"https://mail.aliyun.com/alimail/"
     },{
         "title":"CSDN","url":"http://blog.csdn.net/kcp606"
     },{
-        "title":"知乎","url":"https://www.zhihu.com/"
-    },{
-        "title":"百度网盘","url":"https://pan.baidu.com/disk/home?#list/path=%2F&vmode=list"
-    },{
         "title":"开源中国","url":"https://www.oschina.net/"
+    },{
+        "title":"常用网站","url":"https://github.com/Kuangcp/Notes/blob/master/Website.md"
+    },{
+        "title":"常用软件","url":"https://github.com/Kuangcp/Notes/blob/master/Soft_List.md"
+    },{
+        "title":"常用平台","url":"https://github.com/Kuangcp/Notes/blob/master/Platform.md"
     }
-    // ,{
-    //     "title":" ","url":" "
-    // }
-    // ,{
-    //     "title":" ","url":" "
-    // }
-    // ,{
-    //     "title":" ","url":" "
-    // }
-    // ,{
-    //     "title":" ","url":" "
-    // }
-    // ,{
-    //     "title":" ","url":" "
-    // }
     // ,{
     //     "title":" ","url":" "
     // }
@@ -90,4 +93,34 @@ list={
     // }
 
     ]
+}
+
+$(".menu-element").on('click', function(){
+    type = $(this).data('type')
+    console.log('click', type)
+    
+    if(type=='fun'){
+        insertData(list.fun)
+    }else if(type=='code'){
+        insertData(list.code)
+    }else if(type=='platform'){
+        insertData(list.platform)
+    }
+})
+
+function insertData(lists){
+    temp='<div class="row">'
+    count=0
+    lists.forEach(function(value){
+        count++
+        // console.log(value.title, value.url)
+        temp+='<div class="col-md-'+list.colNum+'" style="width=50%;">'+
+            '<a href="'+value.url+'" style="text-decoration: none;">'+
+            '<div class="box"><div class="text">'+value.title+'</div></div></div></a>'
+        if(count%list.col==0){
+            temp+='</div><div class="row">'
+        }
+    })
+    temp+='</div>'
+    $("#main").html(temp)
 }
