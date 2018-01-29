fe2047bfc9798fad55fab08dbd3b406663d47b05
mythos <kuang.myth@foxmail.com>
2018-01-29 12:40:46 +0800

    add home

---
+++ js/me/main.data.js
@@ -0,0 +1,93 @@
+list={
+    "col":"6",
+    "colNum":"2",
+    data:[{
+        "img":"","title":"Github","url":"https://github.com/Kuangcp"
+    },{
+        "title":"码云","url":"https://gitee.com/kcp1104"
+    },{
+        "title":"七牛云","url":"https://portal.qiniu.com/create"
+    },{
+        "title":"微信","url":"https://wx.qq.com/"
+    },{
+        "title":"WebQQ","url":"http://w.qq.com/"
+    },{
+        "title":"阿里邮箱","url":"https://mail.aliyun.com/alimail/"
+    },{
+        "title":"CSDN","url":"http://blog.csdn.net/kcp606"
+    },{
+        "title":"知乎","url":"https://www.zhihu.com/"
+    },{
+        "title":"百度网盘","url":"https://pan.baidu.com/disk/home?#list/path=%2F&vmode=list"
+    },{
+        "title":"开源中国","url":"https://www.oschina.net/"
+    }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+    // ,{
+    //     "title":" ","url":" "
+    // }
+
+    ]
+}
