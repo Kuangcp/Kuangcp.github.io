44fb96cbd7abcc0ed8213a2200b0247b450f15bd
mythos <kuang.myth@foxmail.com>
2018-02-01 00:25:21 +0800

    终于完成添加页面

--- js/me/main.data.js
+++ js/me/main.data.js
@@ -5,68 +5,47 @@ config={
         "title":"",
         "":"",
     }],
-    type:['fun', 'plarform', 'read', 'code']
+    type:['fun', 'platform', 'read', 'code']
 }
+
 list={
-    fun:[{
-        "title":"知乎","url":"https://www.zhihu.com/"
-    },{
-        "title":"BiliBili","url":"https://www.bilibili.com/#!"
-    },{
-        "title":"网易云音乐","url":"http://music.163.com/"
-    }],
-    platform:[{
-        "title":"微云","url":"https://www.weiyun.com/disk#m=recent"
-    },{
-        "title":"百度网盘","url":"https://pan.baidu.com/disk/home?#list/path=%2F&vmode=list"
-    },{
-        "title":"七牛云","url":"https://portal.qiniu.com/create"
-    },{
-        "title":"微信","url":"https://wx.qq.com/"
-    },{
-        "title":"WebQQ","url":"http://w.qq.com/"
-    },{
-        "title":"ICO转换","url":"https://convertico.com/"
-    }],
-    read:[{
-        "title":"Spring","url":"https://spring.io/"
-    }
-    ],
-    code:[{
-        "title":"Github","url":"https://github.com/Kuangcp"
-    },{
-        "title":"笔记仓库","url":"https://github.com/Kuangcp/Notes/blob/master/SUMMARY.md"
-    },{
-        "title":"码云","url":"https://gitee.com/kcp1104"
-    },{
-        "title":"阿里邮箱","url":"https://mail.aliyun.com/alimail/"
-    },{
-        "title":"Outlook","url":"https://outlook.live.com/mail/#/inbox"
-    },{
-        "title":"CSDN","url":"http://blog.csdn.net/kcp606"
-    },{
-        "title":"开源中国","url":"https://www.oschina.net/"
-    },{
-        "title":"常用网站","url":"https://github.com/Kuangcp/Notes/blob/master/Website.md"
-    },{
-        "title":"常用软件","url":"https://github.com/Kuangcp/Notes/blob/master/Soft_List.md"
-    },{
-        "title":"常用平台","url":"https://github.com/Kuangcp/Notes/blob/master/Platform.md"
-    },{
-        "title":"Miu图标","url":"https://linhpham.me/miu/"
-    },{
-        "title":"阿里图标库","url":"http://www.iconfont.cn/"
-    }
-    // ,{
-    //     "title":"","url":""
-    // }
-    ]
-    // https://www.shiyanlou.com/user/60578/
-    // https://console.bce.baidu.com/bae/?_=1492954589734#/bae/app/list
-    // https://www.codacy.com/projects
-    // https://translate.google.cn/
-    // https://www.gitbook.com/@kuangcp/dashboard
-    // https://home.console.aliyun.com/new#/
-    // https://bitbucket.org/
-    // https://gitlab.com/
-}
+    fun:[
+      {"title":"知乎","url":"https://www.zhihu.com/"},
+      {"title":"BiliBili","url":"https://www.bilibili.com/#!"},
+      {"title":"网易云音乐","url":"http://music.163.com/"},
+      {"title":"腾讯视频","url":"https://v.qq.com/"},
+  ],
+    platform:[
+      {"title":"微云","url":"https://www.weiyun.com/disk#m=recent"},
+      {"title":"百度网盘","url":"https://pan.baidu.com/disk/home?#list/path=%2F&vmode=list"},
+      {"title":"七牛云","url":"https://portal.qiniu.com/create"},
+      {"title":"微信","url":"https://wx.qq.com/"},
+      {"title":"WebQQ","url":"http://w.qq.com/"},
+      {"title":"ICO转换","url":"https://convertico.com/"},
+      {"title":"阿里云","url":"https://home.console.aliyun.com/new#/"},
+      {"title":"百度云","url":"https://console.bce.baidu.com/bae/?_=1492954589734#/bae/app/list"},
+  ],
+    read:[
+      {"title":"Spring","url":"https://spring.io/"},
+  ],
+    code:[
+      {"title":"Github","url":"https://github.com/Kuangcp"},
+      {"title":"笔记仓库","url":"https://github.com/Kuangcp/Notes/blob/master/SUMMARY.md"},
+      {"title":"码云","url":"https://gitee.com/kcp1104"},
+      {"title":"阿里邮箱","url":"https://mail.aliyun.com/alimail/"},
+      {"title":"Outlook","url":"https://outlook.live.com/mail/#/inbox"},
+      {"title":"CSDN","url":"http://blog.csdn.net/kcp606"},
+      {"title":"开源中国","url":"https://www.oschina.net/"},
+      {"title":"常用网站","url":"https://github.com/Kuangcp/Notes/blob/master/Website.md"},
+      {"title":"常用软件","url":"https://github.com/Kuangcp/Notes/blob/master/Soft_List.md"},
+      {"title":"常用平台","url":"https://github.com/Kuangcp/Notes/blob/master/Platform.md"},
+      {"title":"Miu图标","url":"https://linhpham.me/miu/"},
+      {"title":"阿里图标库","url":"http://www.iconfont.cn/"},
+      {"title":"实验楼","url":"https://www.shiyanlou.com/user/60578/"},
+      {"title":"codacy","url":"https://www.codacy.com/projects"},
+      {"title":"Google翻译","url":"https://translate.google.cn/"},
+      {"title":"GitBook","url":"https://www.gitbook.com/@kuangcp/dashboard"},
+      {"title":"BitBucket","url":"https://bitbucket.org/"},
+      {"title":"Gitlab","url":"https://gitlab.com/"},
+  ],
+  }
